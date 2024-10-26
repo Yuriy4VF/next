1e06ef6cfc43fc11bfaf13d9d36c5b920bbbe68c
YuriyEG <you@example.com>
2024-10-26 21:22:09 +0300

    refact: pages/api/register.ts has been refactored

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,24 +1,52 @@
 import prisma from "../../lib/prisma";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const ERROR_MESSAGES = {
+  invalidInput: "Invalid input",
+  internalServerError: "Internal server error",
+  databaseError: "Database error",
+};
+
+const sendErrorResponse = (res, status, message) => {
+  return res.status(status).json({ error: message });
+};
+
+const createUser = async (username, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword,
+    },
+  });
+};
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { username, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    try {
-      const user = await prisma.user.create({
-        data: {
-          username,
-          password: hashedPassword,
-        },
-      });
-      res.status(201).json(user);
-    } catch (error) {
-      res.status(500).json({ error: "Database error" });
-    }
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const { username, password } = req.body;
+
+  if (!username || !password || password.length < 6) {
+    return sendErrorResponse(res, 400, ERROR_MESSAGES.invalidInput);
+  }
+
+  try {
+    const user = await createUser(username, password);
+
+    if (!process.env.JWT_SECRET) {
+      return sendErrorResponse(res, 500, ERROR_MESSAGES.internalServerError);
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse(res, 500, ERROR_MESSAGES.databaseError);
   }
 }
